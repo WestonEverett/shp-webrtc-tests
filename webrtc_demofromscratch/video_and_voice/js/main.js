da413943cc7cf71648aa7112b21ebb6d574b823e
Weston Everett <westoneverett0@gmail.com>
2021-09-29 16:01:01 +0100

    Add Screenshare

--- webrtc_demofromscratch/video_and_voice/js/main.js
+++ webrtc_demofromscratch/video_and_voice/js/main.js
@@ -2,21 +2,50 @@
 
 // Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
-  audio: false,
+  audio: true,
   video: true
 };
 
 const localVideo = document.querySelector('video');
 let localStream;
 
-function handleSuccess(mediaStream) {
+function handleVideoSuccess(mediaStream) {
   localStream = mediaStream;
   localVideo.srcObject = mediaStream;
 }
 
-function handleFailure(error) {
+function handleVideoFailure(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
+function handleStreamSuccess(stream) {
+  startButton.disabled = true;
+  const video = document.getElementById('localShare');
+  video.srcObject = stream;
+
+  // demonstrates how to detect that the user has stopped
+  // sharing the screen via the browser UI.
+  stream.getVideoTracks()[0].addEventListener('ended', () => {
+    errorMsg('The user has ended sharing the screen');
+    startButton.disabled = false;
+  });
+}
+
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', () => {
+  navigator.mediaDevices.getDisplayMedia({video: true})
+      .then(handleStreamSuccess, handleStreamFailure);
+});
+
+function handleStreamFailure(error) {
+  errorMsg(`getDisplayMedia error: ${error.name}`, error);
+}
+
 navigator.mediaDevices.getUserMedia(constraints)
-  .then(handleSuccess).catch(handleFailure);
+  .then(handleVideoSuccess).catch(handleVideoFailure);
+
+
+
+if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+startButton.disabled = false;
+}
